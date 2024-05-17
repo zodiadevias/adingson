3926259e9e5c57457be0878658a3b4bcdd02b5cb
Admin <Admin@DESKTOP-I1B6ILM>
2024-05-18 00:03:05 +0800

    created project

---
+++ HRTS2/src/main/java/com/mycompany/hrts2/HRTS2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hrts2;
+
+/**
+ *
+ * @author Admin
+ */
+public class HRTS2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
